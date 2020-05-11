58913429d960c249ed387294c3c0498c035a5f03
jeyroik <jeyroik@gmail.com>
2020-05-11 22:46:45 +0300

    added use skill op

---
+++ src/components/jsonrpc/JunkmanUseSkill.php
@@ -0,0 +1,39 @@
+<?php
+namespace junkman\components\jsonrpc;
+
+use junkman\interfaces\extensions\IExtensionUseSkill;
+use junkman\interfaces\IJunkman;
+use junkman\interfaces\skills\ISkill;
+
+/**
+ * Class JunkmanUseSkill
+ *
+ * params:
+ *  junkman_name
+ *  skill_name
+ *
+ * @method junkmanRepository()
+ * @method skillRepository()
+ *
+ * @package junkman\components\jsonrpc
+ * @author jeyroik@gmail.com
+ */
+class JunkmanUseSkill extends JunkmanAddSkill
+{
+    /**
+     * @param IJunkman|IExtensionUseSkill $junkman
+     * @param ISkill $skill
+     */
+    protected function doAction(IJunkman &$junkman, ISkill $skill)
+    {
+        $junkman->useSkill($skill->getName(), $junkman, []);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSubjectForExtension(): string
+    {
+        return 'junkman.operation.junkman.use.skill';
+    }
+}
