f1d72ab4025dccfd253520fab34fc4f9cc9ce9e2
jeyroik <jeyroik@gmail.com>
2020-05-11 22:41:04 +0300

    fix

--- src/components/locations/AdjacentDoorOpen.php
+++ src/components/locations/AdjacentDoorOpen.php
@@ -1,6 +1,7 @@
 <?php
 namespace junkman\components\locations;
 
+use extas\components\Item;
 use junkman\interfaces\IJunkman;
 use junkman\interfaces\locations\ILocation;
 use junkman\interfaces\locations\ILocationAdjacent;
@@ -13,11 +14,24 @@ use junkman\interfaces\locations\ILocationAdjacent;
  * @package junkman\components\locations
  * @author jeyroik@gmail.com
  */
-class AdjacentDoorOpen
+class AdjacentDoorOpen extends Item
 {
+    /**
+     * @param IJunkman $junkman
+     * @param ILocationAdjacent $adjacent
+     * @param ILocation $currentLocation
+     */
     public function __invoke(IJunkman &$junkman, ILocationAdjacent $adjacent, ILocation $currentLocation): void
     {
         $junkman->setLocation($adjacent->getLocation());
         $this->junkmanRepository()->update($junkman);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getSubjectForExtension(): string
+    {
+        return 'junkman.location.adjacent.door.open';
     }
 }
